5b47a0872369d2cd01998506d20490efb94cedcf
Mark Neil Tanap <markneilvtanapv2@gmail.com>
2026-09-12 17:42:28 +0800

    frontend api request handled

---
+++ client/src/api/api.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_DEV_API_URL
+    : import.meta.env.VITE_PROD_API_URL;
+
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
+export default api;
