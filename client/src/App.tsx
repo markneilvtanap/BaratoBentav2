e22b8a546e95564c6418f2c824d35cd6821b4529
Mark Neil Tanap <markneilvtanapv2@gmail.com>
2026-09-12 16:47:34 +0800

    intial baratobentav2 codes ;)

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
